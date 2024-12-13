325ef94379765b7ec3b86046b284c7a0e3d781e8
Younas Mahar <mrmahar611@gmail.com>
2024-12-13 20:56:33 +0500

    Assignment-1-Q1

---
+++ Assignment-1.py
@@ -0,0 +1,7 @@
+
+print("Name : Muhammad Younas")
+print("Father Name : Dhani Bakhsh")
+print("Status : Student")
+print("Course : Bachlor in Computer Science")
+print("Age : 21 Years")
+print("City : Rahim Yar Khan")
